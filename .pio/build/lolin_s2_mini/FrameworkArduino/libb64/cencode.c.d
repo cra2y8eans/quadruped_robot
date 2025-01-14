cdae86d3be856e58fe38b9e939990b32ccf1ad73
cra2y8eans <crazybeans@163.com>
2025-01-15 01:46:28 +0800

    main

---
+++ .pio/build/lolin_s2_mini/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/lolin_s2_mini/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/crazy/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/crazy/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
